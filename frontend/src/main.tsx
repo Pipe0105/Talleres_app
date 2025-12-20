f624d21bce9dc589bd6619914f0fe3a335195a73
Pipe0105 <juanfelipegomez0105@gmail.com>
2025-12-20 14:50:36 -0500

    ds

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,19 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import App from "./App";
 import "./index.css";
 import { AuthProvider } from "./context/AuthContext";
-import { AppThemeProvider } from "./context/ThemeContext";
+import { appTheme } from "./theme";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <AppThemeProvider>
+    <ThemeProvider theme={appTheme}>
+      <CssBaseline />
       <BrowserRouter>
         <AuthProvider>
           <App />
         </AuthProvider>
       </BrowserRouter>
-    </AppThemeProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
